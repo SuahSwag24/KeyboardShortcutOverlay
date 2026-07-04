37c833fa371422005601553a972b9aec0df028f0
SuahSwag24 <suahswag24@gmail.com>
2026-07-04 20:33:59 +0800

    #3 fix unknown key when shortcut check

--- src/components/keyboard_listener.py
+++ src/components/keyboard_listener.py
@@ -1,5 +1,6 @@
 from PyQt6.QtCore import QObject, pyqtSignal
 from pynput import keyboard
+from exceptions.keyboard_exception import UnknownKeyError
 import utils.shortcut_loader_utils as shortcut_loader
 from utils.key_utils import format_keys
 
@@ -42,7 +43,10 @@ class KeySignalEmitter(QObject):
         if canonical not in self.keys_pressed:
             self.keys_pressed.add(canonical)
             self.keys_changed.emit(self.keys_pressed)
-            self.check_shortcuts_executed()
+            try:
+                self.check_shortcuts_executed()
+            except UnknownKeyError as e:
+                print(f"Warning: {e}")
 
     def on_release(self, key):
         if not self.listener:
